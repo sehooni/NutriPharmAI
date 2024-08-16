f78e2ed44fa26e97c9fd0bcaa20c657466f22aac
SEHOON PARK <74sehoon@gmail.com>
2024-08-16 21:08:27 +0900

    process save

--- web/seperate_work.py
+++ web/seperate_work.py
@@ -7,11 +7,11 @@ pb = Predibase(api_token="...")
 #     input_prompt = f"저의 성별은 {sex}, 나이는 {age}입니다.\n 제가 지금 먹고 있는 영양제 제품들은 {pill}이고,\n각 영양제의 성분은 {component}입니다.\n {quest}"
 #     return input_prompt
 
-#chg로 주성분 가져오기
+chat = '이렇게 영양제를 복용한다면 어떤 성분이 일일 상한섭취량을 초과하나요?'
 comp = '비타민B1: 0.6, 비타민B2: 0.7, 나이아신 (비타민B3): 7.5, 판토텐산 (비타민B5): 2.5, 비타민B6: 0.75, 비오틴 (비타민B7): 15.0, 엽산 (비타민B9): 200.0, 아연: 4.25, 셀레늄: 27.5, 크롬: 15.0, 코엔자임Q10: 100.0 '
         
 
-def call_model(sex, age, pill, comp, chat):
+def call_model(sex, age, pill):
     lorax_client = pb.deployments.client("solar-1-mini-chat-240612")
     input_prompt = f"저의 성별은 {sex}, 나이는 {age}입니다.\n 제가 지금 먹고 있는 영양제 제품들은 {pill}이고,\n각 영양제의 성분은 {comp}입니다.\n {chat}"
     output = lorax_client.generate(input_prompt, adapter_id="medicine_suggest_model/1", max_new_tokens=100).generated_text
@@ -36,10 +36,10 @@ with gr.Blocks(title="pill assistant") as demo:
         pill = gr.Dropdown(
             ["락포핏", "얼라이브종합비타민", "밀크시슬"], multiselect=True, label="영양제", info="복용 중인 영양제를 선택하여 주세요!"
         )
-        chat = gr.Textbox(label="Chat", placeholder='이렇게 영양제를 복용한다면 어떤 성분이 일일 상한섭취량을 초과하나요?', inputs='이렇게 영양제를 복용한다면 어떤 성분이 일일 상한섭취량을 초과하나요?')
+        # chat = gr.Textbox(label="Chat", placeholder='이렇게 영양제를 복용한다면 어떤 성분이 일일 상한섭취량을 초과하나요?', inputs='이렇게 영양제를 복용한다면 어떤 성분이 일일 상한섭취량을 초과하나요?')
         # chat = '이렇게 영양제를 복용한다면 어떤 성분이 일일 상한섭취량을 초과하나요?'
         # comp = '비타민B1: 0.6, 비타민B2: 0.7, 나이아신 (비타민B3): 7.5, 판토텐산 (비타민B5): 2.5, 비타민B6: 0.75, 비오틴 (비타민B7): 15.0, 엽산 (비타민B9): 200.0, 아연: 4.25, 셀레늄: 27.5, 크롬: 15.0, 코엔자임Q10: 100.0 '
-        # prompt = sentence_builder()
+        # # prompt = sentence_builder()
         submit = gr.Button("생성하기")
 
     with gr.Row():
@@ -59,7 +59,7 @@ with gr.Blocks(title="pill assistant") as demo:
 
         # quest_item_img = gr.Image(label='퀘스트 아이템', elem_id='quest_item')
 
-    submit.click(fn=call_model, inputs=[sex, age, pill, comp, chat], outputs=[output])
+    submit.click(fn=call_model, inputs=[sex, age, pill], outputs=[output])
     # submit.click(fn=sentence_builder, inputs=[sex, age, pill, comp, prompt]],)
 
 demo.launch(share=True)
