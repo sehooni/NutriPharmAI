0fc3235395bc508ae096427e2b4cae2c9247955d
SEHOON PARK <74sehoon@gmail.com>
2024-08-16 14:47:18 +0900

    진행사항 저장

--- web/seperate.py
+++ web/seperate.py
@@ -1,7 +1,7 @@
 import gradio as gr
 
-# def
 
+# def call_model(text, )
 
 _HEADER_ = '''
 <h2><b>💊 Pill_assistant: 약물 보조 AI 💊 </b></h2><h2>
@@ -17,6 +17,7 @@ with gr.Blocks(title="pill assistant") as demo:
             tall = gr.Textbox(label="키(신장)", placeholder='당신의 키(신장)를 넣어주세요!')
             weight = gr.Textbox(label="몸무게", placeholder='당신의 몸무게를 넣어주세요!')
             sex = gr.Radio(["남성", "여성"], label="성별", info='당신의 성별을 선택해주세요!')
+            age = gr.Radio(["어린이 (3~8세)", "청소년 (9~18세)", "성인 (19세 이상)", "노인 (65세 이상)"], label="나이", info='당신의 나이를 선택해주세요!')
             fill = gr.Dropdown(
                 ["락포핏", "얼라이브종합비타민", "밀크시슬"], multiselect=True, label="영양제", info="복용 중인 영양제를 선택하여 주세요!"
             )
